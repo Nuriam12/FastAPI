378cbc6907550736f95d01e2d9ef8f8d66792a4c
Nuriam12 <nuriam1998@hotmail.com>
2025-10-20 14:18:35 -0500

    avanze final

--- app/routers/web.py
+++ app/routers/web.py
@@ -1,14 +1,28 @@
 from fastapi.templating import Jinja2Templates
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, Depends,HTTPException,status
 from fastapi.responses import HTMLResponse
-#import requests
 import aiohttp
+from starlette.responses import RedirectResponse,Response
+from app.token import verify_token
 
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="app/templates")
 
 url = "http://localhost:8000"
+
+def get_current_user(request:Request):
+    token = request.cookies.get("access_token")
+    print("entre",token)
+    if not token:
+        return None
+    else :
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return verify_token (token,credentials_exception)
 
 @router.get("/")
 def home (request:Request):
@@ -63,8 +77,42 @@ def Login_web(request:Request):
     "login.html", {"request": request, "msj":msj}    
     )
 
+@router.get("/salir")
+def Login_web(response:Response , request:Request):
+    msj = ""
+    response = RedirectResponse("/",status_code=302)
+    response.delete_cookie("access_token")
+    return templates.TemplateResponse(
+    "login.html", {"request": request, "msj":msj}    
+    )
 
 @router.post("/Login_web")
-async def Login_web(request: Request):
+async def Login_web(responde:Response, request: Request):
     form = await request.form()
+    usuario = {
+        "username" : form.get('username'),
+        "password" : form.get('password'),
+    } 
+    url_post = f"{url}/login/"
+    async with aiohttp.ClientSession() as session:
+        response = await session.request(method="post",url=url_post,data=usuario)
+        response_json = await response.json()
+        print("\n ->",response_json)
+        if 'access_token' not in response_json:
+             msj = "Usuario o  contraseña incorrecto"
+             return templates.TemplateResponse(
+            "login.html", {"request": request, "msj":msj}    
+            )
+        response = RedirectResponse("/",status_code=302)
+        response.set_cookie(key="access_token",value=response_json["access_token"])
+        return response
 
+
+@router.get("/mostrar_usuarios")
+def mostrar_usuarios(request: Request,current_user= Depends(get_current_user)):
+    msj = ""
+    if current_user:
+        return templates.TemplateResponse("mostrar_usuarios.html",{"request" : request, "msj" :msj})
+    #return templates.TemplateResponse("Login.html",{"request":request , "msj" : "Aun no esta autenticado para ingresar"})
+    response = RedirectResponse("/",status_code=302)
+    return response
